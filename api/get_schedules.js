e64135c3d1f409ea03f6fde8abbac3b9da290955
caraaink <136210717+caraaink@users.noreply.github.com>
2025-03-22 18:40:32 +0700

    Update get_schedules.js

--- api/get_schedules.js
+++ api/get_schedules.js
@@ -4,14 +4,11 @@ import { Redis } from '@upstash/redis';
 export default async function handler(req, res) {
   try {
     const redis = new Redis({
-      url: process.env.UPSTASH_REDIS_REST_URL,
-      token: process.env.UPSTASH_REDIS_REST_TOKEN,
+      url: process.env.KV_URL,
+      token: process.env.KV_REST_API_TOKEN,
     });
 
-    // Ambil jadwal dari Upstash (key: 'schedules')
     const schedules = await redis.get('schedules');
-
-    // Jika tidak ada jadwal, kembalikan array kosong
     res.status(200).json({ schedules: schedules || [] });
   } catch (error) {
     console.error('Error fetching schedules from Upstash:', error);
