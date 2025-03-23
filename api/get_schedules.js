3596b567d3da34816ef6d0cae14047412fc985f1
caraaink <136210717+caraaink@users.noreply.github.com>
2025-03-23 11:07:27 +0700

    Update get_schedules.js

--- api/get_schedules.js
+++ api/get_schedules.js
@@ -3,11 +3,11 @@ const { kv } = require('@vercel/kv');
 const SCHEDULE_KEY = 'schedules';
 
 module.exports = async (req, res) => {
-  try {
-    const schedules = (await kv.get(SCHEDULE_KEY)) || [];
-    res.status(200).json({ schedules });
-  } catch (error) {
-    console.error('Error fetching schedules:', error);
-    res.status(500).json({ error: 'Failed to fetch schedules' });
-  }
+    try {
+        const schedules = (await kv.get(SCHEDULE_KEY)) || [];
+        res.status(200).json({ schedules });
+    } catch (error) {
+        console.error('Error fetching schedules:', error);
+        res.status(500).json({ error: 'Failed to fetch schedules' });
+    }
 };
