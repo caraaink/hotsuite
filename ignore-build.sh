226720dd57a3b39decf1fd4febd15cd75a3b9ffb
caraaink <136210717+caraaink@users.noreply.github.com>
2025-03-27 13:31:04 +0700

    Add files via upload

--- ignore-build.sh
+++ ignore-build.sh
@@ -2,8 +2,8 @@
 # Periksa apakah ada perubahan di folder 'ig'
 if git diff --name-only HEAD^ HEAD | grep -q "^ig/"; then
   echo "Perubahan terdeteksi di folder 'ig', build dibatalkan."
-  exit 1 # Batalkan build dengan error
+  exit 0 # 0 berarti build dilewati
 else
   echo "Tidak ada perubahan di folder 'ig', lanjutkan build."
-  exit 0 # Lanjutkan build dengan sukses
+  exit 1 # 1 berarti build dilanjutkan
 fi
