c06ae8b69b7462032914c157448038729449f61c
caraaink <136210717+caraaink@users.noreply.github.com>
2025-03-27 12:18:35 +0700

    Update ignore-build.sh

--- ignore-build.sh
+++ ignore-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Memeriksa apakah ada perubahan di folder 'ig' dibandingkan dengan commit sebelumnya
+# Memeriksa apakah ada perubahan di folder 'ig' pada commit terakhir
 if [ -n "$(git log -1 --name-only --pretty=format: | grep '^ig/')" ]; then
-    echo "Perubahan terdeteksi di folder 'ig', build dibatarkan."
+    echo "Perubahan terdeteksi di folder 'ig', build dibatalkan."
     exit 0  # Exit 0 untuk melewati build
 else
     echo "Tidak ada perubahan di folder 'ig', lanjutkan build."
