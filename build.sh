dbe53ed9d0a2617719abd2350e2f9277a2bd6096
caraaink <136210717+caraaink@users.noreply.github.com>
2025-03-27 23:58:42 +0700

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Memeriksa apakah ada perubahan di folder 'ig' pada commit terakhir
-if [ -n "$(git log -1 --name-only --pretty=format: | grep '^ig/')" ]; then
-    echo "Perubahan terdeteksi di folder 'ig', build dibatalkan."
-    exit 1
-else
-    echo "Tidak ada perubahan di folder 'ig', lanjutkan build."
-    exit 0
-fi
