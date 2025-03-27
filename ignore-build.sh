77a521ff385bc65ccdad63aaf022380f2a4ad1f1
caraaink <136210717+caraaink@users.noreply.github.com>
2025-03-27 22:09:58 +0700

    Add files via upload

--- ignore-build.sh
+++ ignore-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Memeriksa apakah ada perubahan di folder 'ig' pada commit terakhir
 if [ -n "$(git log -1 --name-only --pretty=format: | grep '^ig/')" ]; then
     echo "Perubahan terdeteksi di folder 'ig', build dibatalkan."
     exit 1
