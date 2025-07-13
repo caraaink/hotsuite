3b9ff62c6aa9aa51ba41da7cd3e721ad6ff7356a
caraaink <136210717+caraaink@users.noreply.github.com>
2025-07-13 08:16:38 +0700

    Update get_file_content.js

--- api/get_file_content.js
+++ api/get_file_content.js
@@ -51,7 +51,7 @@ module.exports = async (req, res) => {
           }
 
           const cacheKey = `file_content:${path}`;
-          await kv.set(cacheKey, parsedContent, { ex: 3600 }); // Cache selama 1 jam
+          await kv.set(cacheKey, parsedContent, { ex: 86400000 }); // Cache selama 1 jam
           return { path, content: parsedContent };
         } catch (error) {
           console.error(`Error fetching file content for path ${path}:`, error.response?.data || error.message);
